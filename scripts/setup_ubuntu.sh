f6b6df2709e4145589376717e395db7c46ad24bc
Jason <callmebob@gmail.com>
2021-08-30 20:50:33 -0600

    Added sudo in setup script.

--- scripts/setup_ubuntu.sh
+++ scripts/setup_ubuntu.sh
@@ -1,5 +1,5 @@
 # Install apt packages
-apt-get install build-essential cmake valgrind libczmq-dev ruby-dev cppcheck
+sudo apt-get install build-essential cmake valgrind libczmq-dev ruby-dev cppcheck
 
 # Install gem packages
-gem install ceedling
+sudo gem install ceedling

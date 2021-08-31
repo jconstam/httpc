1ef49f809720c0e0fbe92747163e81c33cddcd72
Jason <callmebob@gmail.com>
2021-08-30 20:35:56 -0600

    Added basic project files.

---
+++ scripts/setup_ubuntu.sh
@@ -0,0 +1,5 @@
+# Install apt packages
+apt-get install build-essential cmake valgrind libczmq-dev ruby-dev cppcheck
+
+# Install gem packages
+gem install ceedling
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#ifdef TEST
+int test_main(int argc, char *argv[])
+#else
+int main(int argc, char *argv[])
+#endif
+{
+    return 0;
+}
